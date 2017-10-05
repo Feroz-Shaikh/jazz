365ecaf486babc3b382562be97b17be4eb58cdaa
Cloud User <ec2-user@ip-10-0-165-250.ec2.internal>
2017-10-05 01:55:32 -0400

    changes made for js

--- jazz-web/config/toastmessages.ts
+++ jazz-web/config/toastmessages.ts
@@ -1,7 +1,7 @@
 export const toastMessage = {
-    "error0":"Something went wrong while fetching data",
+    "error0":"Server cannot be reached at the moment",
     "error401":"Invalid credentials",
-    "error403":"Unable to recognize user. Please login again",
+    "error403":"Forbidden",
     "errorInternet":"No internet connection",
     "errorDefault":"Unexpected error",
     "dataNull":"Data not available",
@@ -13,14 +13,6 @@ export const toastMessage = {
         "error404":"Something went wrong while we were fetching your services!",
         "error500":"Something went wrong while we were fetching your services!"
     },
-    "serviceMetrics":{
-        "dataNull":"Data not available",
-        "error400":"Something went wrong while we were fetching the service details!",
-        "error401":"Token expired",
-        "error404":"Something went wrong while we were fetching your services!",
-        "error500":"Something went wrong while we were fetching your services!",
-        "customMsg":"Incorrect selection combination"
-    },
     "serviceDetail":{
         "dataNull":"Something went wrong while we were fetching the service details!",
         "error400":"Something went wrong while we were fetching the service details!",
@@ -29,7 +21,7 @@ export const toastMessage = {
         "error500":"Something went wrong while we were fetching the service details!"
     },
     "serviceDelete":{
-        "success":" is being deleted.",
+        "success":"\"{service_name}\" is being deleted.",
         "error400":"Unable to delete service at this time.",
         "error401":"Token expired",
         "error404":"Unable to delete service at this time.",
@@ -44,12 +36,11 @@ export const toastMessage = {
         "error500":"Something went wrong while we were fetching your services!"
     },
     "createService":{
-        "success":"Your service will be available shortly. You can track the progress on service page ",
+        "success":"SUCCESS!\nYour service will be available shortly. You can track the progress on service page\nYou can refer :\nhttps://tmobile.atlassian.net/wiki/display/CLP/Getting+Started+with+Service+Development+using+Serverless+Framework",
         "error400":"Looks like we couldn't create your service. Please try again.",
         "error401":"Token expired",
         "error404":"Looks like we couldn't create your service. Please try again.",
-        "error500":"Looks like we couldn't create your service. Please try again.",
-        "error0":"Looks like we couldn't create your service. Please try again."
+        "error500":"Looks like we couldn't create your service. Please try again."
     },
     "envDetail":{
         "dataNull":"Something went wrong while we were fetching the environment details!",
@@ -68,31 +59,11 @@ export const toastMessage = {
         "error0":"Your logout failed, seems like we can't reach our servers."
     },
     "login":{
-        "fail":"Is that you? Your login failed. So please try again..",
+        "fail":"Your login failed, seems like we can't reach our servers.",
         "error400":"Your login failed. Seems like something went wrong.",
         "error401":"Your login failed. Seems like something went wrong.",
         "error404":"Your login failed. Seems like something went wrong.",
         "error500":"Your login failed. Seems like something went wrong.",
         "error0":"Your login failed, seems like we can't reach our servers."
-    },
-    "error404":{
-        "errorTxt":"Uh-oh, looks like you took a wrong turn, let's see if we can turn around.",
-        "errorHdr":"Are you lost?"
-    },
-    "serviceAvailability":{
-        "error0":"Looks like we are having trouble reaching our servers. Please try later!"
-    },
-    "aduser":{
-        "error0": "Looks like we are having trouble reaching our servers. Please try later!"
-    },
-    "slackChannel":{
-        "error0": "Looks like we are having trouble reaching our servers. Please try later!"
-    },
-    "updateObj":{
-        "success":"updated succesfully",
-        "error400":"Something went wrong while updating the service details. Please try again",
-        "error500":"Something went wrong while updating the service details. Please try again",
-
-       
     }
 };
